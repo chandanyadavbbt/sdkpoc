40785b9e7a524f1d90cb60aad3102ac14249e213
chandanyadavbbt <chandan.yadav@blackbasil.tech>
2024-06-25 18:40:54 +0530

    adding widget in server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello, I am a sample SDK' });
 });
 
+// Endpoint to serve the widget script from Netlify
+app.get('/widget.js', (req, res) => {
+  // Replace 'https://your-netlify-site.netlify.app' with your Netlify site's URL
+  res.redirect('https://dreamy-fenglisu-5872fe.netlify.app/widget.js');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
